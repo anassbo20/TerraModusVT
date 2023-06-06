f399fb81abf00e186043dd31756b14260b264f75
anassbo20 <101249842+anassbo20@users.noreply.github.com>
2023-06-06 16:01:17 +0100

    Update TerraModusV1ApplicationTests.java

--- src/test/java/com/example/terramodusv1/TerraModusV1ApplicationTests.java
+++ src/test/java/com/example/terramodusv1/TerraModusV1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.terramodusv1;
+package com.example.terramodusvt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TerraModusvcApplicationTests {
+class TerraModusVTApplicationTests {
 
     @Test
     void contextLoads() {
